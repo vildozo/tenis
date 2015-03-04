b4f3ed7c5f2920c76b1e0036c8e26b1acf13d412
Javier <vildozo@outlook.com>
2015-03-04 17:38:38 -0400

    puse un end

--- app.rb
+++ app.rb
@@ -10,48 +10,25 @@ post '/jugar' do
 	@anota=params[:anota]
 	@jugador1=params[:jugador1]
 	@jugador2=params[:jugador2]
-	@puntos1=params[:puntos1]
-	@puntos2=params[:puntos2]
+	@puntos1=params[:puntos1].to_i
+	@puntos2=params[:puntos2].to_i
 	@ganador=params[:ganador]
 	
-	if @puntos1=="6"
+	if(@puntos1==@puntos2+2)
 		redirect "/ganador"
-	end
-	if @puntos2=="6"
+	else
+		if(@puntos2==@puntos1+2)
 		redirect "/ganador2"
-	end
-
-	if @puntos1=="5" && @puntos2=="5"
-			@puntos1="4"
-			@puntos2="4"
-	end
-
-	if @anota=="1"
-		@puntos=@puntos1.to_i
-		@puntos+=1
-		@puntos1=@puntos.to_s
-		if @puntos1=="4" && @puntos2<="2"
-			redirect "/ganador"
 		end
-
-	elsif @anota=="2"
-		@puntos=@puntos2.to_i
-		@puntos+=1
-		@puntos2=@puntos.to_s
-		if @puntos2=="4" && @puntos1<="2"
-			redirect "/ganador2"
-		end
-
-		if @puntos1=="5" && @puntos2=="5"
-			@puntos1="4"
-			@puntos2="4"
-		end
-
 	end
-	if @puntos1=="3" && @puntos2=="3" 
-		@puntos1="5"
-		@puntos2="5"
+
+	if(@puntos2==@puntos1 && @puntos2==5)
+		@puntos1=4
+		@puntos2=4
 	end
+
+
+
 
 		
 	
