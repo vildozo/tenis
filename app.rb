8a48c1ae07fb22b795e5a6b7bcbf47f4671fd23b
sergioarispejulio <sergio_arispe@hotmail.com>
2015-03-04 20:00:43 -0400

    Funcionando

--- app.rb
+++ app.rb
@@ -7,12 +7,10 @@ end
 post '/jugar' do
 	@punto_tenis={0=>"0",1=>"15",2=>"30",3=>"40",4=>"Deuce",5=>"A",6=>"Ganador"}
 	@punto=0
-	@anota=params[:anota]
 	@jugador1=params[:jugador1]
 	@jugador2=params[:jugador2]
 	@puntos1=params[:puntos1].to_i
 	@puntos2=params[:puntos2].to_i
-	@ganador=params[:ganador]
 	
 	if(@puntos1==@puntos2+2)
 		redirect "/ganador"
